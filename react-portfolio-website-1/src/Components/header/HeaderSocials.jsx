058310dc62e2b5823d4cd63ccc2a72996c167a31
ayush <ayushgupta220503@gmail.com>
2022-06-11 15:04:33 +0530

    NAV bar created

--- react-portfolio-website-1/src/Components/header/HeaderSocials.jsx
+++ react-portfolio-website-1/src/Components/header/HeaderSocials.jsx
@@ -5,9 +5,9 @@ import {SiHackerrank} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
-        <a href="https://hackerrank.com" target="_blank"><SiHackerrank/></a>
+        <a href="https://www.linkedin.com/in/ayush-gupta2205/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/AyushGupta2114" target="_blank"><FaGithub/></a>
+        <a href="https://www.hackerrank.com/ag8815" target="_blank"><SiHackerrank/></a>
     </div>
   )
 }
