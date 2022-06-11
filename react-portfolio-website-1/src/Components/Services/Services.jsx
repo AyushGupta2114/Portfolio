71bba54efd6509906aa8bd42d3dabd15cc303385
ayush <ayushgupta220503@gmail.com>
2022-06-12 00:48:30 +0530

    projects section started

--- react-portfolio-website-1/src/Components/Services/Services.jsx
+++ react-portfolio-website-1/src/Components/Services/Services.jsx
@@ -1,8 +1,93 @@
 import React from 'react'
 import './services.css'
+import {BsCheck2All} from 'react-icons/bs'
 const Services = () => {
   return (
-    <section id='services'>Section</section>
+    <section id='services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <div
+        className="container services__container">
+          <article className="service">
+          <div className="service__head">
+            <h3>UI/UX design</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+                elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+                elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+          </article>
+          <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+
+          </article>
+          <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            
+          </ul>
+          </article>
+      </div>
+    </section>
   )
 }
 
