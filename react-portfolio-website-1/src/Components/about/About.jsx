bfd64b8f7981dafa971c2763637dfd3f71635bf1
ayush <ayushgupta220503@gmail.com>
2022-06-11 23:27:21 +0530

    Experience done

--- react-portfolio-website-1/src/Components/about/About.jsx
+++ react-portfolio-website-1/src/Components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me-about.jpg'
+import ME from '../../assets/carrom2.jpeg'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
@@ -9,16 +9,18 @@ const About = () => {
     <section id='about'>
       <h5>
       Get to Know
-    </h5>
-    <h2>About Me</h2>
-    <div className="container about__container">
-      <div className="about__me">
-        <img src={ME} alt="About Image"/>
+      </h5>
+      <h2>About Me
+      </h2>
+    <div className='container about__container'>
+      <div className='about__me'>
+        <div className='about__me-image'>
+        <img src={ME} alt='About Image'/>
+        </div>
       </div>
-    </div>
-
-    <div className="about__content">
-      <div className="about__cards">
+    
+    <div className='about__content'>
+      <div className='about__cards'>
         <article className='about__card'>
           <FaAward className='about__icon'/>
           <h5>Experience</h5>
@@ -41,6 +43,7 @@ const About = () => {
       </p>
 
       <a href='#contact' className='btn btn-primary'>Lets' Talk</a>
+    </div>
     </div>
     </section>
     
--- react-portfolio-website-1/src/Components/experience/Experience.jsx
+++ react-portfolio-website-1/src/Components/experience/Experience.jsx
@@ -1,8 +1,92 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I have</h5>
+      <h2>&nbsp;My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>
+            Frontend Development
+          </h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>HTML & CSS</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>C Programming</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Beginner</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>Python</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>C++</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className='experience__backend'>
+        <h3>
+            Non-Programming Language
+          </h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>AutoCad 3D</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>SolidWorks</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>UX/UI Design</h4>
+              <small className='text-light'>Beginner</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>MySQL</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
