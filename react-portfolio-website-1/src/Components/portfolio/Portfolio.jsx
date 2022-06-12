ad6a57b5c5de966ea9f7a84514fc9d5e5f1fdc3a
ayush <ayushgupta220503@gmail.com>
2022-06-12 18:07:05 +0530

    Cards swiper completed

--- react-portfolio-website-1/src/Components/portfolio/Portfolio.jsx
+++ react-portfolio-website-1/src/Components/portfolio/Portfolio.jsx
@@ -1,8 +1,80 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/real-time.jpg'
+import IMG2 from '../../assets/portfoliomine.jpg'
+import IMG3 from '../../assets/solid_works.jpg'
+import IMG5 from '../../assets/tic3.jfif'
+import IMG6 from '../../assets/webclone.jpg'
+import IMG4 from '../../assets/facedet.jpg'
 const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt=""/>
+          </div>
+            <h3>This is a portfolio item title</h3>
+           <div className='portfolio__item-cta'>
+            <a href='https://github.com/AyushGupta2114' className='btn' target='_blank'>Github</a>
+            <a href='https://github.com/AyushGupta2114' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+        <article className='portfolio__item_2'>
+          <div className='portfolio__item-image_2'>
+            <img src={IMG2} alt=""/>
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+            <a href='https://github.com/AyushGupta2114' className='btn' target='_blank'>Github</a>
+            <a href='https://github.com/AyushGupta2114' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+        <article className='portfolio__item_3'>
+          <div className='portfolio__item-image_3'>
+            <img src={IMG3} alt=""/>
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+            <a href='https://github.com/AyushGupta2114' className='btn' target='_blank'>Github</a>
+            <a href='https://github.com/AyushGupta2114' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+        <article className='portfolio__item_4'>
+          <div className='portfolio__item-image_4'>
+            <img src={IMG4} alt=""/>
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+            <a href='https://github.com/AyushGupta2114' className='btn' target='_blank'>Github</a>
+            <a href='https://github.com/AyushGupta2114' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+        <article className='portfolio__item_5'>
+          <div className='portfolio__item-image_5'>
+            <img src={IMG5} alt=""/>
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+            <a href='https://github.com/AyushGupta2114' className='btn' target='_blank'>Github</a>
+            <a href='https://github.com/AyushGupta2114' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+        <article className='portfolio__item_6'>
+          <div className='portfolio__item-image_6'>
+            <img src={IMG6} alt=""/>
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+            <a href='https://github.com/AyushGupta2114' className='btn' target='_blank'>Github</a>
+            <a href='https://github.com/AyushGupta2114' className='btn btn-primary' target='_blank'>Live Demo</a>
+           </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- react-portfolio-website-1/src/Components/testimonials/Testimonials.jsx
+++ react-portfolio-website-1/src/Components/testimonials/Testimonials.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './testimonials.css'
-const testimonials = () => {
-  return (
-    <section id='testimonials'>Testimonials</section>
-  )
-}
+import AVTR1 from '../../assets/Google.png'
+import AVTR2 from '../../assets/logo2.png'
+import AVTR3 from '../../assets/logo2.png'
 
-export default testimonials
+
+import { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+// import required modules
+import { EffectCards } from "swiper";
+
+const data=[
+  {
+    avatar:AVTR1,
+    name:'Tin Show',
+    review:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit itaque optio officiis laboriosam enim quod cupiditate delectus ea, dolorem unde fugit maxime sequi quia dolor! Dignissimos voluptaibus rerum quas sequice"
+  },
+  {
+    avatar: AVTR2,
+    name:'Shatta Wale',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit itaque optio officiis laboriosam enim quod cupiditate delectus ea, dolorem unde fugit maxime sequi quia dolor! Dignissimos voluptatibus rerum quas sequice'
+  },
+  {
+    avatar: AVTR3,
+    name:'Kwame Despite',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit itaque optio officiis laboriosam enim quod cupiditate delectus ea, dolorem unde fugit maxime sequi quia dolor! Dignissimos voluptatibus rerum quas sequice'
+  },
+]
+
+const Testimonials = () => {
+  return (
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+         effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        >
+        {
+          data.map(({avatar,name,review},index)=>{
+            return(
+              <SwiperSlide key={index} className='testimonial'>
+              <div class="client__avatar">
+                <img src={avatar}/>
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>
+                  {review}
+                </small>
+            </SwiperSlide>
+            )
+
+          })
+        }
+      </Swiper>
+    </section>
+  )
+      }
+
+
+export default Testimonials
