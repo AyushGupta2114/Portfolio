46250ec3088da3763e9f21a51da4c802f8320c75
ayush <ayushgupta220503@gmail.com>
2022-06-14 00:45:35 +0530

    Completed every thing except hosting

--- react-portfolio-website-1/src/Components/nav/Nav.jsx
+++ react-portfolio-website-1/src/Components/nav/Nav.jsx
@@ -5,18 +5,30 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
-import {useState} from 'react'
+import {useState,Fragment} from 'react'
 
 const Nav = () => {
   const[activeNav,setActiveNav] = useState('')
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () =>{
+     if(window.scrollY >= 5170){
+       setColorchange(true);
+     }
+     else{
+       setColorchange(false);
+     }
+  };
+  window.addEventListener('scroll', changeNavbarColor);
   return (
-    <nav>
+    <Fragment>
+    <nav className={colorChange ? 'navbar colorChange' : 'nav'}>
       <a href="#" onClick={()=>setActiveNav('#')}className={activeNav==='#' ? 'active':''}><AiOutlineHome/></a>
       <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav==='#about' ? 'active':''}><AiOutlineUser/></a>
       <a href="#experience" onClick={()=>setActiveNav('#experience')} className={activeNav==='#experience' ? 'active':''}><BiBook/></a>
       <a href="#services" onClick={()=>setActiveNav('#services')} className={activeNav==='#services' ? 'active':''}><RiServiceLine/></a>
       <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav==='#contact' ? 'active':''}><BiMessageSquareDetail/></a>
     </nav>
+    </Fragment>
   )
 }
 
