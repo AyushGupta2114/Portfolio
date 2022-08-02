34eb7d5e953e9c6456d43f28e3b6bbd01b73a623
ayush <ayushgupta220503@gmail.com>
2022-08-02 22:18:01 +0530

    quotes removed due to dislike cartifications added

--- React-Portfolio/src/Components/testimonials/Testimonials.jsx
+++ React-Portfolio/src/Components/testimonials/Testimonials.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './testimonials.css'
-import AVTR1 from '../../assets/Google.png'
+import AVTR1 from '../../assets/microsoft.png'
 import AVTR2 from '../../assets/logo2.png'
-import AVTR3 from '../../assets/logo2.png'
+import AVTR3 from '../../assets/Google.png'
 
 
 import { useRef, useState } from "react";
@@ -19,26 +19,26 @@ import { EffectCards } from "swiper";
 const data=[
   {
     avatar:AVTR1,
-    name:'Martin Fowler',
-    review:'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.' 
+    name:'Artificial intelligence for Students',
+    review:'" Any fool can write code that a computer can understand. Good programmers write code that humans can understand. "' 
   },
   {
     avatar: AVTR2,
-    name:'Eagleson’s Law',
-    review:'Any code of your own that you haven’t looked at for six or more months might as well have been written by someone else.'
+    name:'',
+    review:'" Any code of your own that you haven’t looked at for six or more months might as well have been written by someone else. "'
   },
   {
     avatar: AVTR3,
     name:'Robert C. Martin',
-    review:'It is not the language that makes programs appear simple. It is the programmer that make the language appear simple!'
+    review:'" It is not the language that makes programs appear simple. It is the programmer that make the language appear simple! "'
   },
 ]
 
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      <h5>Some</h5>
-      <h2>Quotes</h2>
+      <h5>Online</h5>
+      <h2>Certifications</h2>
 
       <Swiper className="container testimonials__container"
          effect={"cards"}
