bd70515cbf75295755429f609cc6024a145d06e3
AyushGupta2114 <ayushgupta220503@gmail.com>
2022-11-12 11:17:51 +0530

    leetcode icons added in header socials😀

--- React-Portfolio/src/Components/header/HeaderSocials.jsx
+++ React-Portfolio/src/Components/header/HeaderSocials.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {SiHackerrank} from 'react-icons/si'
+import {GoMarkGithub} from 'react-icons/go'
+import {FaHackerrank} from 'react-icons/fa'
+import {SiLeetcode} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/ayush-gupta2205/" target="_blank"><BsLinkedin size={25}/></a>
-        <a href="https://github.com/AyushGupta2114" target="_blank"><FaGithub size={25}/></a>
-        <a href="https://www.hackerrank.com/ag8815" target="_blank"><SiHackerrank size={25}/></a>
+        <a href="https://leetcode.com/AyushGupta2114/" target="_blank"><SiLeetcode size={25}/></a>
+        <a href="https://github.com/AyushGupta2114" target="_blank"><GoMarkGithub size={25}/></a>
+        <a href="https://www.hackerrank.com/ag8815" target="_blank"><FaHackerrank size={25}/></a>
     </div>
   )
 }
