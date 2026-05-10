e17e4abcadac7e6bcc7baa6adeb783e11e51a00d
AyushGupta2114 <ayushgupta220503@gmail.com>
2026-05-10 21:03:38 +0530

    did all the changes

--- React-Portfolio/src/Components/header/HeaderSocials.jsx
+++ React-Portfolio/src/Components/header/HeaderSocials.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import { GoGithub } from "react-icons/go";
-import {FaHackerrank} from 'react-icons/fa'
-import {SiLeetcode} from 'react-icons/si'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { FaHackerrank } from 'react-icons/fa'
+import { SiLeetcode } from 'react-icons/si'
+
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/ayush-gupta2205/" target="_blank" rel='noreferrer'><BsLinkedin size={25}/></a>
-        <a href="https://leetcode.com/AyushGupta2114/" target="_blank" rel='noreferrer'><SiLeetcode size={25}/></a>
-        <a href="https://github.com/AyushGupta2114" target="_blank" rel='noreferrer'><GoGithub size={25}/></a>
-        <a href="https://www.hackerrank.com/ag8815" target="_blank" rel='noreferrer'><FaHackerrank size={25}/></a>
+      <a href="https://www.linkedin.com/in/ayush-gupta2205/" target="_blank" rel="noreferrer">
+        <BsLinkedin size={25}/>
+      </a>
+      <a href="https://leetcode.com/AyushGupta2114/" target="_blank" rel="noreferrer">
+        <SiLeetcode size={25}/>
+      </a>
+      <a href="https://github.com/AyushGupta2114" target="_blank" rel="noreferrer">
+        <FaGithub size={25}/>
+      </a>
+      <a href="https://www.hackerrank.com/ag8815" target="_blank" rel="noreferrer">
+        <FaHackerrank size={25}/>
+      </a>
     </div>
   )
 }
