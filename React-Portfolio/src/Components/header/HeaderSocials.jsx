ea8b70444687409dab400f67cab974c86180d7e4
AyushGupta2114 <ayushgupta220503@gmail.com>
2026-05-10 20:31:53 +0530

    changes manually

--- React-Portfolio/src/Components/header/HeaderSocials.jsx
+++ React-Portfolio/src/Components/header/HeaderSocials.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
-import {GoMarkGithub} from 'react-icons/go'
+import { GoGithub } from "react-icons/go";
 import {FaHackerrank} from 'react-icons/fa'
 import {SiLeetcode} from 'react-icons/si'
 const HeaderSocials = () => {
@@ -8,7 +8,7 @@ const HeaderSocials = () => {
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/ayush-gupta2205/" target="_blank" rel='noreferrer'><BsLinkedin size={25}/></a>
         <a href="https://leetcode.com/AyushGupta2114/" target="_blank" rel='noreferrer'><SiLeetcode size={25}/></a>
-        <a href="https://github.com/AyushGupta2114" target="_blank" rel='noreferrer'><GoMarkGithub size={25}/></a>
+        <a href="https://github.com/AyushGupta2114" target="_blank" rel='noreferrer'><GoGithub size={25}/></a>
         <a href="https://www.hackerrank.com/ag8815" target="_blank" rel='noreferrer'><FaHackerrank size={25}/></a>
     </div>
   )
